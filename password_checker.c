a11d45380d62d11c095fa755cec7a53eea165d50
shivam0-0-ui <sg9815086742@gmail.com>
2026-08-06 15:50:17 +0530

    pass_checker.c

---
+++ password_checker.c
@@ -0,0 +1,48 @@
+# my-first-project.py
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(){
+  std::string password;
+
+  std::cout<<"enter the password:";
+  std::cin>>password;
+
+  bool hasNumber = false;
+  bool hasSpecial = false;
+  bool hasUpper = false;
+  bool hasLower = false;
+
+  if(password.length()>=8){
+
+    for(int i=0; i<password.length(); ++i){
+
+      char ch = password[i];
+
+      if(isdigit(ch)){
+        hasNumber = true;
+      }
+
+      if(!isalnum(ch)){
+        hasSpecial = true;
+      }
+
+      if(islower(ch)){
+        hasLower = true;
+      }
+
+      if(isupper(ch)){
+        hasUpper = true;
+      }
+    }
+
+    if(hasNumber && hasSpecial && hasLower && hasUpper){
+      std::cout<<"valid password"<<std::endl;
+    }
+    else{
+      std::cout<<"invalid password"<<std::endl;
+
+      if(!hasNumber){
+        std::cout<<"number is needed\n";
+      }
